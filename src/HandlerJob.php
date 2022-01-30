af8bf681f7a60e304f85c7f40f661f594c08a3df
Jonathan Fontes <jonathan.alexey@icloud.com>
2022-01-30 11:07:41 +0000

    FIX: HandlerJob Interface must accept ConfigurationContext in first paramenter

--- src/HandlerJob.php
+++ src/HandlerJob.php
@@ -2,9 +2,10 @@
 
 namespace Gravatalonga\WatcherChecker;
 
+use Gravatalonga\WatcherChecker\Entities\ConfigurationContext;
 use Gravatalonga\WatcherChecker\Entities\Context;
 
 interface HandlerJob
 {
-    public function handle(array $config, Context $context): ResponseStatus;
+    public function handle(ConfigurationContext $config, Context $context): ResponseStatus;
 }
